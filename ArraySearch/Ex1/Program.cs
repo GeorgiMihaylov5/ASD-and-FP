5bd248ca3617c240897df106b094c96e5ca4c964
Георги Михайлов <gogogm59@outlook.com>
2021-10-18 13:43:29 +0300

    add project

---
+++ ArraySearch/Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int[] arr = { 2, 1, 7, 5, 9, 3, 6, 4, 8 };
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == n)
+                    {
+                        Console.WriteLine($"Found number: {n}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ArraySearch/Ex2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int[] arr = { 2, 1, 7, 5, 9, 3, 6, 4, 8 };
+                int n = int.Parse(Console.ReadLine());
+
+                Search(arr, n);
+            }
+        }
+        static void Search(int[] arr, int n)
+        {
+            int[] l = arr.Take(arr.Length / 2).ToArray();
+            int[] r = arr.Skip(arr.Length / 2).ToArray();
+
+            if (l.Contains(n))
+            {
+                if (l.Length > 1)
+                {
+                    Search(l, n);
+                }
+                else if (l[0] == n)
+                {
+                    Console.WriteLine($"Found number: {l[0]}");
+                    return;
+                }
+            }
+            else
+            {
+                if (r.Length > 1)
+                {
+                    Search(r, n);
+                }
+                else if (r[0] == n)
+                {
+                    Console.WriteLine($"Found number: {r[0]}");
+                    return;
+                }
+            }
+
+        }
+    }
+}
